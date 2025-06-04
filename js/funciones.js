79aad4402241f99a444c140bf12bc14308ab4360
Ana3 <anaensayo2025@gmail.com>
2025-06-04 11:13:42 +0200

    js para menú desplegable

---
+++ js/funciones.js
@@ -0,0 +1,26 @@
+const navegacion = document.querySelector("nav");
+
+/*
+const abrir = document.querySelector(".abrir");
+const cerrar = document.querySelector(".cerrar");
+*/
+const botones = document.querySelectorAll(".abrir,.cerrar"); //en este saco, abrir sería 0 y cerrar 1
+
+
+
+/*
+botones[0].addEventListener("click", function(){
+    navegacion.className = "desplegado";
+});
+
+botones[1].addEventListener("click", function(){
+    navegacion.className = "";
+});
+*/
+
+
+for(let i = 0; i < botones.length; i++){
+    botones[i].addEventListener("click", function(){
+    navegacion.className = i == 0 ? "desplegado" : "";
+});
+}
