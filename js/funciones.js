fb8db13d36d19da8164578e0362d127ebdf7edff
Ana3 <anaensayo2025@gmail.com>
2025-06-04 11:32:12 +0200

    web listosite

--- js/funciones.js
+++ js/funciones.js
@@ -17,8 +17,6 @@ botones[1].addEventListener("click", function(){
     navegacion.className = "";
 });
 */
-
-
 for(let i = 0; i < botones.length; i++){
     botones[i].addEventListener("click", function(){
     navegacion.className = i == 0 ? "desplegado" : "";
